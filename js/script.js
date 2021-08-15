376fb8df990eaf72b175e89bf907f1c8b789bf87
hossam22770 <67165788+hossam22770@users.noreply.github.com>
2021-08-15 21:21:22 +0200

    ok!

---
+++ js/script.js
@@ -0,0 +1,18 @@
+$('.move').hover(function()
+{
+    $('.head-marker').css('width', '140px')
+});
+$('.move').mouseout(function()
+{
+    $('.head-marker').css('width', '80px')
+});
+
+
+// $('.option1').hover(function()
+// {
+//     $('.layout').slideDown(1000,function()
+//     {
+//         $('.descreption').fadeIn(1000)
+//     })
+// });
+
