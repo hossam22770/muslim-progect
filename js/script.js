67d40d062f56a8401202f46f893508719bc31b96
hossam22770 <67165788+hossam22770@users.noreply.github.com>
2021-08-19 17:17:14 +0200

    move

--- js/script.js
+++ js/script.js
@@ -14,7 +14,7 @@ $(document).ready(function()
     $('body').css("overflow" ,"auto");
   });
 });
-new WOW().init();
+
 $('.option1').hover(function()
 {
     $('.layout').slideDown(1000,function()
@@ -22,4 +22,30 @@ $('.option1').hover(function()
         $('.descreption').fadeIn(1000)
     })
 });
+new WOW().init();
+let w =$('#demo').offset().top
+$(window).scroll( function()
+{
+   let s=$(window).scrollTop();
+   if(s>w)
+   {
+     $('.navbar').css('backgroundColor', 'rgb(237, 254, 244)')
+   }
+   else
+   {
+    $('.navbar').css('backgroundColor', '#fff')
+   }
+})
+var lastScrollTop = 0;
+$(window).scroll(function(event){
+   var st = $(this).scrollTop();
+   if (st > lastScrollTop){
+       // downscroll code
+       $('.navbar').css('opacity', '0')
 
+   } else {
+      // upscroll code
+      $('.navbar').css('opacity', '1')
+   }
+   lastScrollTop = st;
+});
